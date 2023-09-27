730789975d215114d36522647cfbc5e8cc889acf
root <root@WanZ-Desktop.localdomain>
2023-09-27 14:02:54 +0800

    implemented speech to text with speech_recognition

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import os
 import sys
+import _thread
+import threading
+
+import speech_recognition
 
 from pythonosc import udp_client
 import pyperclip as cb
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMenu, QAction, QHBoxLayout, QVBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMenu, QAction, QListWidget, QHBoxLayout, QVBoxLayout, QSizePolicy
 from PyQt5.QtGui import QIcon, QFont, QCursor
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.Qt import QTextEdit
 
 from extra_data import ICON_PATH
@@ -20,6 +23,7 @@ except ImportError:
 
 
 class BrassicaGUI(QWidget):
+    speech_recognized_text = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
@@ -28,6 +32,18 @@ class BrassicaGUI(QWidget):
         self.osc_prefix_input = "/chatbox/input"
 
         self.auto_clear = True
+
+        self.speech_listening = False
+        self.speech_listening_lock = threading.Lock()
+        self.stop_listening = None
+
+        self.speech_recognizer = speech_recognition.Recognizer()
+        self.speech_microphone = speech_recognition.Microphone()
+        # calibrate at start
+        with self.speech_microphone as source:
+            self.speech_recognizer.adjust_for_ambient_noise(source)
+
+        print(speech_recognition.Microphone.list_microphone_names())
 
         self.init_gui()
 
@@ -56,6 +72,18 @@ class BrassicaGUI(QWidget):
         self.talkButton.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         self.talkButton.clicked.connect(self.on_push_talk_button)
 
+        # talk button menu
+        self.talk_button_menu = QMenu(self)
+        self.talk_button_menu.setObjectName("talk_button_menu")
+
+        # self.talk_button_device_list = QListWidget()
+        # self.talk_button_device_list.currentItemChanged.connect(self.on_talkbutton_device_changed)
+        #
+        # self.talk_button_menu.addActions([self.talk_button_device_list])
+
+        self.talkButton.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.talkButton.customContextMenuRequested.connect(self.on_req_talkbutton_menu)
+
         self.vbox1.addWidget(self.talkButton)
 
         self.textBox = QTextEdit(self)
@@ -63,6 +91,8 @@ class BrassicaGUI(QWidget):
         self.textBox.setMinimumSize(400, 100)
         self.textBox.setAlignment(Qt.AlignTop)
         self.textBox.textChanged.connect(self.on_text_changed)
+
+        self.speech_recognized_text.connect(self.handle_speech_recognition_result)
 
         self.vbox1.addWidget(self.textBox)
 
@@ -121,6 +151,12 @@ class BrassicaGUI(QWidget):
     def on_text_changed(self):
         cb.copy(self.textBox.toPlainText())
 
+    def handle_speech_recognition_result(self, recognized_text):
+        self.textBox.setText(recognized_text)
+        with self.speech_listening_lock:
+            self.speech_listening = False
+            self.toggle_speech_listening()
+
     def on_push_clear_button(self):
         last_text = self.textBox.toPlainText()
         self.textBox.setText("")
@@ -129,16 +165,66 @@ class BrassicaGUI(QWidget):
         self.textBox.setFocus()
 
     def on_push_send_button(self):
-        self.send_content_to_osc(self.textBox.toPlainText())
-        if self.auto_clear:
-            self.textBox.setText("")
+        self.send_content_to_osc()
         # set focus back to textBox after any button click
         self.textBox.setFocus()
 
     def on_req_sendbutton_menu(self):
         self.send_button_menu.popup(QCursor.pos())
+        self.textBox.setFocus()
 
     def on_push_talk_button(self):
+        with self.speech_listening_lock:
+            if self.speech_listening:
+                self.stop_listening(wait_for_stop=True)
+                self.speech_listening = False
+                self.toggle_speech_listening()
+                self.speech_recognized_text.emit("")
+                return
+            self.speech_listening = True
+            self.toggle_speech_listening()
+            print("[on_push_talk_button] locked")
+
+        def process_speech():
+            with self.speech_microphone as source:
+                audio = self.speech_recognizer.listen(source)
+            print("[process_speech] start")
+            recognized_text = ""
+            try:
+                recognized_text = self.speech_recognizer.recognize_google(audio, language="zh-CN")
+                print("[process_speech] recognize completed")
+            except speech_recognition.UnknownValueError:
+                print("[process_speech] speech_recognition Unknown error")
+            except speech_recognition.RequestError as e:
+                print(f"[process_speech] request failed: {e}")
+            print(f"[process_speech] rec_text: {recognized_text}")
+            print("[process_speech] 1")
+            print(f"[process_speech] textbox: {self.textBox.toPlainText()}")
+            try:
+                self.speech_recognized_text.emit(recognized_text)
+            except Exception as e:
+                print(f"[process_speech] setText error: {e}")
+            print("[process_speech] return")
+
+        print("[listen_speech] 2")
+        _thread.start_new_thread(process_speech, ())
+
+        self.textBox.setFocus()
+        print("[on_push_talk_button] return")
+
+    def on_req_talkbutton_menu(self):
+        self.talk_button_menu.popup(QCursor.pos())
+        self.textBox.setFocus()
+
+    def on_talkbutton_device_changed(self):
+        pass
+
+    def toggle_speech_listening(self):
+        if self.speech_listening:
+            self.talkButton.setText("LISTENING..")
+        else:
+            self.talkButton.setText("⚪ TALK")
+
         self.textBox.setFocus()
 
     def toggle_auto_clear(self, enabled):
@@ -151,9 +237,11 @@ class BrassicaGUI(QWidget):
         # set focus back to textBox
         self.textBox.setFocus()
 
-    def send_content_to_osc(self, content):
+    def send_content_to_osc(self):
+        content = self.textBox.toPlainText()
         self.osc_client.send_message(self.osc_prefix_input, [content, True])
-
+        if self.auto_clear:
+            self.textBox.setText("")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
